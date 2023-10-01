f986647aba656faf785100ea651543b6a8f2f853
jakub_vala <jakub.vala@betsys.com>
2023-10-01 15:55:54 +0200

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,16 @@ import {
 } from './ui/controller';
 import { loggerMiddleware } from './ui/middleware/logger.middleware';
 import { createInviteeSchema } from './ui/schema/request/create-invitee.request.dto';
+import * as cors from 'cors'
+
+const serverCors = cors({
+  origin: 'https://www.jbwedding.info',
+})
 
 const server = express();
 server.use(express.json());
 server.use(loggerMiddleware);
+server.use(serverCors);
 server.all('/hello', healthCheckController);
 server.post('/invitee', controllerWithReqValidation({ bodySchema: createInviteeSchema }, createInviteeController));
 server.use(errorController);
